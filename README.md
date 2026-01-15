51b9a203effb784f1d6ac752cf807093d317dcf2
ath331 <kstnftk@naver.com>
2026-01-15 23:07:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DX11
+DirectX11
---
+++ main/pch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+#include "Values.h"
+#include "Types.h"
+#include "Struct.h"
+
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#include <windows.h>
+#include <assert.h>
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+
+#include <DirectXTex/DirectXTex.h>
+#include <DirectXTex/DirectXTex.inl>
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib,"DirectXTex\\DirectXTex_debug.lib")
+#else
+#pragma comment(lib,"DirectXTex\\DirectXTex.lib")
+#endif
